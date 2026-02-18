62a2d33c4932f3d042ba4b72421570c2018c14db
Yonatan Azmir <yonatanezmir@gmail.com>
2026-02-19 01:29:48 +0300

    resolve ModuleNotFoundError issue

--- ai-doc-assistant/loaders/document_loader.py
+++ ai-doc-assistant/loaders/document_loader.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader
-def load_pdf():
-    loader = PyPDFLoader(r"C:\Users\Yoni\OneDrive\Desktop\Langchain  AI Chatbot\Database Recovery Techniques.pdf")
+def load_pdf(file_path):
+    loader = PyPDFLoader(file_path)
     return loader.load()
 
